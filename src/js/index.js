1ee4675e6c450e30b481e6231c8d3a6fc383be6d
Cory <coryp@jkdesign.com>
2018-11-07 11:53:49 -0500

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+const testArr = ["hello", "hello again", "cool"];
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+const filesToWatch = ["./*.php", "./dist/*.js", "./dist/*.css"];
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 3000,
+      files: filesToWatch,
+      proxy: "http://vagrant.host"
+    })
+  ]
+};
